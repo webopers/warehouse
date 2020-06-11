6e181381ca7b04c5db962d77cc83f004b1ff3590
Encacap <encacap@gmail.com>
2020-06-11 20:04:00 +0700

    authentication with Firebase Auth

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
 import firebase from "./firebase-config.js";
 
-console.log(firebase);
+const developmentEnvironment = window.location.href.split("/")[2] === "warehouse.webopers.com";
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (!user) {
+		if (developmentEnvironment) Location.href = "/accounts/login/";
+		else Location.href = "/login/";
+	}
+});
