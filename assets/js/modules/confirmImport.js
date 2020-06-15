1121ef063c94b8bef7e02a78bb450f3dacabd389
Encacap <encacap@gmail.com>
2020-06-15 09:44:00 +0700

    refactoring logs database

--- assets/js/modules/confirmImport.js
+++ assets/js/modules/confirmImport.js
@@ -1,17 +1,18 @@
-const onConfirmImportClicked = (warehouse, logs, updated, name, time) => {
+const onConfirmImportClicked = (warehouse, author, time) => {
+	const log = warehouse.child("logs");
 	const warehouseItems = !localStorage.getItem("warehouseItems") ? {} : JSON.parse(localStorage.getItem("warehouseItems"));
 	Object.keys(warehouseItems).forEach((itemID) => {
-		warehouse.push(warehouseItems[itemID]);
-		logs.push({
+		warehouse.child("items").push(warehouseItems[itemID]);
+		log.child("detail").push({
 			action: "import",
 			time,
 			content: warehouseItems[itemID].item.name,
-			name,
+			author,
 		});
 	});
+	log.child("updatedTime/item").set(time);
+	log.child("updatedTime/log").set(time);
 	localStorage.clear();
-	updated.child("logUpdate").set(time);
-	updated.child("updated").set(time);
 	window.location.href = "/";
 };
 
